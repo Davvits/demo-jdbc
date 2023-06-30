1c9b6780b56e797bc2b10acb6931669e1e5ec34d
davi <kaliflamel@gmail.com>
2023-06-30 01:47:24 -0300

    add classe Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,43 +1,13 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
-import db.DbException;
+import model.entities.Department;
 
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = null;
-		Statement st = null;
-		
-		try {
-			conn = DB.getConnection();
-			conn.setAutoCommit(false);
-			
-			st = conn.createStatement();
-			int rown1 = st.executeUpdate("UPDATE seller SET BaseSalary = 0");
-			//int rown2 = st.execute(null);
-			System.out.println(rown1);
-			conn.commit();
-		}
-		catch(SQLException e) {
-			try {
-				conn.rollback();
-			}
-			catch(SQLException el ) {
-				throw new DbException(el.getMessage());
-			}
-			
-			e.printStackTrace();;
-		}
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		Department d = new Department(1,"seinen");
+		System.out.println(d);
 	}
 
 }
