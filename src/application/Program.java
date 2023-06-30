a1ec5de7b1f9c08aa844e551741614e4d48f4ce2
davi <kaliflamel@gmail.com>
2023-06-30 19:44:34 -0300

    add FindByDepartment method

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,14 +3,27 @@ package application;
 
 import model.dao.SellerDao;
 import model.entities.Seller;
+
+import java.util.List;
+
 import model.dao.FactoryDao;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		SellerDao sellerDao = FactoryDao.createSellerDao();
+		System.out.println("===== TEST: 1: seller findById ======");
+		
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
+		System.out.println();
+		
+		System.out.println("=========== TEST: 2 seller findByDepar ===");
+		Department dep1 = new Department(2,null);
+		List<Seller> list = sellerDao.findByDepartment(dep1);
+		list.stream().forEach(System.out::println);
 	}
 
 }
